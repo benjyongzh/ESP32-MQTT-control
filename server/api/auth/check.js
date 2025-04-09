7ceeaacbbee60980d73417c799a325dc76b232d0
benjyongzh <benj_yongzh@hotmail.com>
2025-04-09 15:35:57 +0800

    Try single file routing

--- server/api/auth/check.js
+++
@@ -1,9 +0,0 @@
-import { verifyToken } from "../../middleware/authMiddleware";
-
-export default function handler(req, res) {
-  if (!verifyToken(req)) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
-  return res.status(200).json({ valid: true });
-}
--- server/middleware/authMiddleware.js
+++
@@ -1,6 +0,0 @@
-import cookie from "cookie";
-
-export function verifyToken(req) {
-  const cookies = req.headers.cookie ? cookie.parse(req.headers.cookie) : {};
-  return cookies[process.env.COOKIE_NAME] === process.env.SHARED_TOKEN;
-}
