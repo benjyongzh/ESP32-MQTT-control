208d9de49bc7d48c324954ac029e45744d0b93c6
benjyongzh <benj_yongzh@hotmail.com>
2025-04-09 18:00:05 +0800

    Remove additional layer of authentication

--- client/src/pages/Control.tsx
+++ client/src/pages/Control.tsx
@@ -1,28 +1,21 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 import mqtt, { MqttClient } from 'mqtt';
 import ControlItem from '../components/ControlItem';
 
 export default function Control() {
   const [client, setClient] = useState<MqttClient | null>(null);
-  const navigate = useNavigate();
 
   useEffect(() => {
-    axios.get(`${import.meta.env.VITE_BACKEND_URL}/auth/check`, { withCredentials: true })
-      .then(() => {
-        const mqttClient = mqtt.connect(import.meta.env.VITE_MQTT_CLUSTER_URL, {
-          username: import.meta.env.VITE_MQTT_USERNAME,
-          password: import.meta.env.VITE_MQTT_PASSWORD,
-        });
+    const mqttClient = mqtt.connect(import.meta.env.VITE_MQTT_CLUSTER_URL, {
+      username: import.meta.env.VITE_MQTT_USERNAME,
+      password: import.meta.env.VITE_MQTT_PASSWORD,
+    });
 
-        mqttClient.on('connect', () => {
-            mqttClient.subscribe(import.meta.env.VITE_MQTT_TOPIC_STATUS);
-        });
+    mqttClient.on('connect', () => {
+        mqttClient.subscribe(import.meta.env.VITE_MQTT_TOPIC_STATUS);
+    });
 
-        setClient(mqttClient);
-      })
-      .catch(() => navigate('/login'));
+    setClient(mqttClient);
   }, []);
 
   return (
@@ -31,7 +24,7 @@ export default function Control() {
       <div className="space-x-4">
         <ControlItem client={client} topicControl={import.meta.env.VITE_MQTT_TOPIC_CONTROL} topicStatus={import.meta.env.VITE_MQTT_TOPIC_STATUS} />
       </div>
-      <p className="mt-4 text-lg">{client ? 'Connecting to MQTT...' : '✅ Connected to MQTT broker'}</p>
+      <p className="mt-4 text-lg">{client ? '✅ Connected to MQTT broker' : 'Connecting to MQTT...'}</p>
     </div>
   );
 }
