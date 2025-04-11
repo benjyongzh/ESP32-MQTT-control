329437bcaabdb97f2fda28e30b3d8450e2779a68
benjyongzh <benj_yongzh@hotmail.com>
2025-04-11 09:57:07 +0800

    UI fixes for alert

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Alert, AlertDescription } from "@/components/ui/alert";
 import { ChevronRight, Ban } from "lucide-react";
 import Logo from "@/components/Logo";
 
@@ -24,7 +24,7 @@ export default function Login() {
       navigate("/control");
       setIsLoading(false);
     } catch (err) {
-      setError("Invalid token");
+      setError("Invalid Token");
       setIsLoading(false);
     }
   };
@@ -33,34 +33,35 @@ export default function Login() {
     <div className="base justify-center -mt-5">
       <Logo />
       <h1 className="mb-3">Enter Token</h1>
-      <div className="flex items-center justify-center gap-3">
-        <Input
-          type="password"
-          placeholder="password"
-          value={token}
-          onChange={(e) => setToken(e.target.value)}
-        />
-        <Button
-          size="icon"
-          disabled={isLoading}
-          onClick={handleLogin}
-          className={isLoading ? "btn-disabled" : ""}
-        >
-          {isLoading ? (
-            <span className="loading loading-dots loading-md"></span>
-          ) : (
-            <ChevronRight />
-          )}
-        </Button>
-      </div>
+      <div className="w-full flex flex-col justify-center items-center gap-6">
+        <div className="flex items-center w-full justify-center gap-3">
+          <Input
+            type="password"
+            placeholder="password"
+            value={token}
+            onChange={(e) => setToken(e.target.value)}
+          />
+          <Button
+            size="icon"
+            disabled={isLoading}
+            onClick={handleLogin}
+            className={isLoading ? "btn-disabled" : ""}
+          >
+            {isLoading ? (
+              <span className="loading loading-dots loading-md"></span>
+            ) : (
+              <ChevronRight />
+            )}
+          </Button>
+        </div>
 
-      {error && (
-        <Alert>
-          <Ban className="h-4 w-4" />
-          <AlertTitle>Error</AlertTitle>
-          <AlertDescription>{error}</AlertDescription>
-        </Alert>
-      )}
+        {error && (
+          <Alert className="w-full" variant="destructive">
+            <Ban className="h-4 w-4" />
+            <AlertDescription>{error}</AlertDescription>
+          </Alert>
+        )}
+      </div>
     </div>
   );
 }
