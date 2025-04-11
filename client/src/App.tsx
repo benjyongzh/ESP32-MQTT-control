bd4150a356e22306459f3dea5e1f0889b0ad01b2
benjyongzh <benj_yongzh@hotmail.com>
2025-04-11 09:56:47 +0800

    Add dark theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,23 +2,26 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Control from "./pages/Control";
 import ProtectedRoute from "./pages/ProtectedRoute";
+import { ThemeProvider } from "@/components/theme-provider";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/control"
-          element={
-            <ProtectedRoute>
-              <Control />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/control"
+            element={
+              <ProtectedRoute>
+                <Control />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
